0eebd9d0b6f4f55a5ea65ac6fa2cd7a6ca845dd8
Your Name <you@example.com>
2026-04-13 19:52:03 +0000

    feat(push): add consent fields and casts to LoyaltyCard model

--- app/Models/LoyaltyCard.php
+++ app/Models/LoyaltyCard.php
@@ -15,11 +15,35 @@ class LoyaltyCard extends Model
         'max_stamps',
         'current_stamps',
         'status',
+        'marketing_consent',
+        'marketing_consent_at',
+        'marketing_consent_revoked_at',
+        'sms_marketing_consent',
+        'sms_marketing_consent_at',
+        'sms_marketing_consent_revoked_at',
+        'email_marketing_consent',
+        'email_marketing_consent_at',
+        'email_marketing_consent_revoked_at',
+        'push_consent',
+        'push_consent_at',
+        'push_consent_revoked_at',
     ];
 
     protected $casts = [
-        'current_stamps' => 'integer',
-        'max_stamps'     => 'integer',
+        'current_stamps'                     => 'integer',
+        'max_stamps'                         => 'integer',
+        'marketing_consent'                  => 'boolean',
+        'marketing_consent_at'               => 'datetime',
+        'marketing_consent_revoked_at'       => 'datetime',
+        'sms_marketing_consent'              => 'boolean',
+        'sms_marketing_consent_at'           => 'datetime',
+        'sms_marketing_consent_revoked_at'   => 'datetime',
+        'email_marketing_consent'            => 'boolean',
+        'email_marketing_consent_at'         => 'datetime',
+        'email_marketing_consent_revoked_at' => 'datetime',
+        'push_consent'                       => 'boolean',
+        'push_consent_at'                    => 'datetime',
+        'push_consent_revoked_at'            => 'datetime',
     ];
 
     /*
